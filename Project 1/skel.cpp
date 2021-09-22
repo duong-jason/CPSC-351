6329073df8f6c9578840ba3aac3f30b6553d487e
Jason Duong <reddkingdom@csu.fullerton.edu>
2021-09-21 21:56:43 -0700

    Create skel.cpp

---
+++ Project 1/skel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  return 0;
+}
