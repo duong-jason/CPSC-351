900989d4028c37fc035f94fc9752ae6bc6c0702e
Jason Duong <reddkingdom@csu.fullerton.edu>
2021-11-27 00:53:28 -0800

    Create log.cpp

---
+++ examples/MEMORY/log.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int r;
+  cout << "Enter Value: ";
+  cin >> r;
+  cout << endl << "log2(" << r << ") = " << log2(r) << endl;
+  return 0;
+}
