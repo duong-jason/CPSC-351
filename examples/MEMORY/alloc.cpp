9582230792111cb1a777dc2204e68d8cf9c11b14
Jason Duong <reddkingdom@csu.fullerton.edu>
2021-12-09 00:22:07 -0800

    Update alloc.cpp

--- examples/MEMORY/alloc.cpp
+++ examples/MEMORY/alloc.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 #include <stack>
 
 using namespace std;
@@ -7,6 +8,9 @@ using namespace std;
 vector<int> mem_req; 
 vector<int> h, hole;
 vector<vector<int>> result;
+vector<vector<string>> calc;
+
+struct item { int hole; int pos; };
 
 void ff(vector<int> ohole) {
 	for (int i = 0; i < mem_req.size(); ++i) {
@@ -14,23 +18,28 @@ void ff(vector<int> ohole) {
 		for (int j = 0; j < h.size(); ++j) {
 			if (ohole[j] >= mem_req[i]) {
 				result[0][i] = h[j];
+				calc[0][i] = to_string(ohole[j]) + "-";
+				calc[0][i] += to_string(mem_req[i]);
+				calc[0][i] += "=";
+
 				ohole[j] -= mem_req[i];
+
+				calc[0][i] += to_string(ohole[j]);
+
 				bit = true;
-				goto END;
+				break;
 			}
 		}
-		END: if (!bit) result[0][i] = -1;
+		if (!bit) result[0][i] = -1;
 	}
 }
-
-struct item { int hole; int pos; };
 
 item findmin(stack<item>& save) {
 	item min = save.top();
 	save.pop();
 
 	while (!save.empty()) {
-		if (save.top().hole < min.hole) min = {save.top().hole, save.top().pos};
+		if (save.top().hole <= min.hole) min = {save.top().hole, save.top().pos};
 		save.pop();
 	}
 
@@ -41,14 +50,21 @@ void bf(vector<int> ohole) {
 	stack<item> save;
 	for (int i = 0; i < mem_req.size(); ++i) {
 		for (int j = 0; j < h.size(); ++j) {
-			if (ohole[j] >= mem_req[i]) save.push({h[j], j});
+			if (ohole[j] >= mem_req[i]) save.push({ohole[j], j});
 		}
 
 		if (save.empty()) result[1][i] = -1;
 		else {
 			item min = findmin(save);
 			result[1][i] = h[min.pos];
+
+			calc[1][i] = to_string(ohole[min.pos]) + "-";
+			calc[1][i] += to_string(mem_req[i]);
+			calc[1][i] += "=";
+
 			ohole[min.pos] -= mem_req[i];
+
+			calc[1][i] += to_string(ohole[min.pos]);
 		}
 	}
 }
@@ -58,7 +74,7 @@ item findmax(stack<item>& save) {
 	save.pop();
 
 	while (!save.empty()) {
-		if (save.top().hole > max.hole) max = {save.top().hole, save.top().pos};
+		if (save.top().hole >= max.hole) max = {save.top().hole, save.top().pos};
 		save.pop();
 	}
 
@@ -69,28 +85,42 @@ void wf(vector<int> ohole) {
 	stack<item> save;
 	for (int i = 0; i < mem_req.size(); ++i) {
 		for (int j = 0; j < h.size(); ++j) {
-			if (ohole[j] >= mem_req[i]) save.push({h[j], j});
+			if (ohole[j] >= mem_req[i]) save.push({ohole[j], j});
 		}
 
 		if (save.empty()) result[2][i] = -1;
 		else {
 			item max = findmax(save);
 			result[2][i] = h[max.pos];
+
+			calc[2][i] = to_string(ohole[max.pos]) + "-";
+			calc[2][i] += to_string(mem_req[i]);
+			calc[2][i] += "=";
+
 			ohole[max.pos] -= mem_req[i];
+
+			calc[2][i] += to_string(ohole[max.pos]);
 		}
 	}
 }
 
 int main() {
-	size_t x, y, xsize, ysize, i = 0, k = 0;
+	size_t x, y, xsize, ysize;
 	cout << "Enter Memory Requirements Size: "; cin >> xsize;
 	cout << "Enter Memory Requirements: ";
-	while (i++ < xsize) { cin >> x; mem_req.push_back(x); }
+	while (mem_req.size() < xsize && cin >> x) mem_req.push_back(x);
 	cout << "Enter Memory Hole Size: "; cin >> ysize;
 	cout << "Enter Memory Holes: ";
-	while (k++ < ysize) { cin >> y; h.push_back(y); hole.push_back(y); }
-	cout << "\n\n";
+	while (hole.size() < ysize && cin >> y) { h.push_back(y); hole.push_back(y); }
 
+	for (int i = 0; i < xsize-1; ++i) {
+		calc.push_back({""});
+		for (int j = 0; j < 3; ++j) {
+			calc[i].push_back("");	
+		}
+	}
+
+	cout << "\n\n";
 	for (int i = 0; i < 3; ++i) {
 		result.push_back({});
 		for (int j = 0; j < xsize; ++j) result[i].push_back(0);
@@ -98,12 +128,35 @@ int main() {
 
 	ff(hole); bf(hole); wf(hole);
 
+	int z = 1;
+	cout << "Memory Holes: "; for (size_t h : hole) { cout << h << " ";  } cout << endl;
+	string t; cout << "Process "; for (size_t v : mem_req) { cout << v << " "; t += to_string(v); } cout << endl;
+	for (int i = 0; i < t.length()+11; ++i) { cout << "-"; } cout << endl;
+
 	for (vector<int> l : result) {
+		switch(z) {
+			case 1: cout << "First-Fit | "; break;
+			case 2: cout << "Best-Fit  | "; break;
+			case 3: cout << "Worst-Fit | "; break;
+			default: break;
+		}
 		for (int s : l) {
 			if (s == -1) cout << "wait ";
 			else cout << s << "kb ";
 		}
+		z++;
 		cout << endl;
 	}
+
+	cout << endl << endl;
+	cout << "Calculations: \n--------------------------\n";
+	for (vector<string> c : calc) {
+		for (string s : c) {
+			cout << s << setw(12-s.length()) << "|";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
 	return 0;
 }
