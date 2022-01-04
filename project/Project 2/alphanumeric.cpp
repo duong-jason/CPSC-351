8e75aa532624f168776d8ec6379a2ebf78b7ce83
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-01-04 02:28:47 -0800

    Update alphanumeric.cpp

--- project/Project 2/alphanumeric.cpp
+++ project/Project 2/alphanumeric.cpp
@@ -4,33 +4,28 @@
  */
 
 #include <pthread.h>
-#include <iostream>
-#include <string>
-#include <bits/stdc++.h> // stringstream
-#include <cctype> // isalpha | ispunct | isdigit
+#include <string.h>
+#include <stdio.h>
+#include <cctype> // isalpha | isdigit
 
 using namespace std;
 
 /*
- * @bit     [ halts numeric thread if it starts with an alphabet or an alphabet thread starts with a numeric ]
- *          [ additionally determines if next word needs to be read of not ]
- * @read    [ parses the phrase into tokens (words) ]
- * @word    [ the string to be printed to the console ]
+ * @flag    [ halts numeric thread if it starts with an alphabet or an alphabet thread starts with a numeric ]
+ * @token   [ the current string to be printed to the console ]
  * @alpha   [ parses out words that start with an alphabet ]
  * @numeric [ parses out numeric words that start with a numeric ]
  */
-bool bit[2] = {0, 0};
-
-stringstream read;
-string word;
+bool flag = 0;
+char* token;
 
 void* alpha(void*);
 void* numeric(void*);
 
 void alphalock() {
-    if (!isalpha(word[0])) {
-        bit[0] = bit[1] = true;
-        while (bit[0]) {
+    if (!isalpha(token[0])) {
+        flag = true;
+        while (flag) {
             /*
              * let numeric-thread continue processing
              * while alpha-thread waits for interrupt
@@ -41,9 +36,9 @@ void alphalock() {
 }
 
 void numlock() {
-    if (!isdigit(word[0])) {
-        bit[0] = bit[1] = false;
-        while (!bit[0]) {
+    if (!isdigit(token[0])) {
+        flag = false;
+        while (!flag) {
             /*
              * let alpha-thread continue processing
              * while numeric-thread waits for interrupt
@@ -56,13 +51,11 @@ void numlock() {
 int main(int argc, char* argv[]) {
     if (argc != 2) { // error checking for number of arguments
         fprintf(stderr, "USAGE: %s <message string>\n", argv[0]);
-        exit(-1);
+        return -1;
     }
 
-    // reading the phrase from terminal and storing in a global variable, phrase
-    read << argv[1];
     // initilize parser to read the first word in the phrase
-    read >> word;
+    token = strtok(argv[1], " ");
 
     // creating two concurrent threads (total 3 including parent thread)
     pthread_t t1, t2;
@@ -74,21 +67,25 @@ int main(int argc, char* argv[]) {
 }
 
 void* alpha(void* arg) {
-    do {
+    while (1) {
         alphalock(); // only allow alpha-words to pass
-        if (!bit[1] && read) cout << "alpha: " << word << endl;
-    } while (read >> word);
+        if (!token) break;
+        printf("alpha: %s\n", token);
+        token = strtok(NULL, " ");
+    }
 
-    bit[0] = true; // frees numeric thread
+    flag = true; // frees numeric thread
     pthread_exit(0);
 }
 
 void* numeric(void* arg) {
-    do {
+    while (1) {
         numlock(); // only allow numeric-words to pass
-        if (bit[1] && read) cout << "numeric: " << word << endl;
-    } while (read >> word);
+        if (!token) break;
+        printf("numeric: %s\n", token);
+        token = strtok(NULL, " ");
+    }
     
-    bit[0] = false; // frees alpha thread
+    flag = false; // frees alpha thread
     pthread_exit(0);
 }
