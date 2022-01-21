85e3aac73041ad0d66ba9f3b508a855d06c95e36
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-01-21 11:04:21 -0800

    Update alphanumeric.cpp

--- project/Project 2/alphanumeric.cpp
+++ project/Project 2/alphanumeric.cpp
@@ -4,64 +4,76 @@
  */
 
 #include <pthread.h>
-#include <string.h>
+#include <string.h> // strtok
 #include <stdio.h>
 #include <cctype> // isalpha | isdigit
 
-using namespace std;
-
 /*
- * @state   [ halts numeric thread if it starts with an alphabet or an alphabet thread starts with a numeric ]
+ * @state   [ current state based on the current token read ]
  * @token   [ the current string to be printed to the console ]
+ * wait     [ locks a thread based on the current token read ]
  * @alpha   [ parses out words that start with an alphabet ]
  * @numeric [ parses out numeric words that start with a numeric ]
  */
-enum State { A, N } state;
+
+enum State_t { ALPHA, NUMERIC } state;
 char* token;
 
-#define wait(S)                  \
-	state = S;                   \
-	while (state == S) continue; \
-	if (!token) break;
-
-#define signal(S) \
-	printf("%s %s\n", (S == A ? "alpha:" : "numeric:"), token);
-	
+static void wait(const State_t&);
 void* alpha(void*);
 void* numeric(void*);
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) { // error checking for number of arguments
-        fprintf(stderr, "USAGE: %s <message string>\n", argv[0]);
-        return -1;
+    // error checking for number of arguments
+    if (argc == 2) {
+        token = strtok(argv[1], " "); // initilize parser to read the first word in the phrase
+        pthread_t t1, t2; // creating two concurrent threads (total 3 including parent thread)
+
+        pthread_create(&t1, NULL, alpha, NULL);
+        pthread_create(&t2, NULL, numeric, NULL);
+
+        pthread_join(t1, NULL);
+        pthread_join(t2, NULL);
     }
+    else fprintf(stderr, "%s <message-string>\n", argv[0]);
+}
 
-    token = strtok(argv[1], " "); // initilize parser to read the first word in the phrase
-    pthread_t t1, t2; // creating two concurrent threads (total 3 including parent thread)
-
-    pthread_create(&t1, NULL, alpha, NULL);
-    pthread_create(&t2, NULL, numeric, NULL);
-
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+static void wait(const State_t& curr) {
+    switch (curr) {
+        case ALPHA : {
+            if (isdigit(token[0])) {
+                state = NUMERIC;
+                while (state == NUMERIC) continue;
+            }
+            break;
+        }
+        case NUMERIC : {
+            if (isalpha(token[0])) {
+                state = ALPHA;
+                while (state == ALPHA) continue;
+            }
+            break;
+        }
+        default : break;
+    }
 }
 
 void* alpha(void* arg) {
-	do {
-        if (isdigit(token[0])) { wait(N); } // only allow alpha-words to pass
-		signal(A);
-    } while (token = strtok(NULL, " "));
+    do {
+        wait(ALPHA); // only allow alpha-words to pass
+        printf("alpha: %s\n", (token ? token : "EOS (end-of-string)"));
+    } while ((token = strtok(NULL, " ")) > 0);
 
-    state = N; // frees numeric thread
-    pthread_exit(0);
+    state = NUMERIC; // frees numeric thread
+    pthread_exit(NULL);
 }
 
 void* numeric(void* arg) {
-	do {
-		if (!isdigit(token[0])) { wait(A); } // only allow numeric-words to pass
-		signal(N);
-    } while (token = strtok(NULL, " "));
+    do {
+        wait(NUMERIC); // only allow numeric-words to pass
+        printf("numeric: %s\n", (token ? token : "EOS (end-of-string)"));
+    } while ((token = strtok(NULL, " ")) > 0);
     
-    state = A; // frees alpha thread
-    pthread_exit(0);
+    state = ALPHA; // frees alpha thread
+    pthread_exit(NULL);
 }
